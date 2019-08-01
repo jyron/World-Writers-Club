a83c1b9c02c769878ce73907a2df65de89ee041a
com <walls.jyron@gmail.com>
2019-08-01 00:09:26 -0700

    deployment configs

--- app/__init__.py
+++ app/__init__.py
@@ -6,32 +6,35 @@ import os
 
 db = SQLAlchemy()
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bae.db'
-heroku = Heroku(app)
-db.init_app(app)
+    app.config['SECRET_KEY'] = 'secret'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bae.db'
+    heroku = Heroku(app)
+    db.init_app(app)
     
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
     
-from .models import User, Writing
+    from .models import User, Writing
     
-@login_manager.user_loader
-def load_user(user_id):
+    @login_manager.user_loader
+    def load_user(user_id):
         return User.query.get(int(user_id))
     
         #blueprint for routes that require auth 
-from .auth import auth as auth_blueprint
-app.register_blueprint(auth_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
     
         # blueprint for non-auth parts of app
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
+create_app()
 
 """ 
 so the objects of this app
